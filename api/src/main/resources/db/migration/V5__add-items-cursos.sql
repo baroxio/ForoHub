66aaf79c1ed2f1876230c7187c646f981cb99409
baroxio <jose.rigoberto@gmail.com>
2024-07-07 18:23:10 -0600

    Update V5__add-items-cursos.sql

--- api/src/main/resources/db/migration/V5__add-items-cursos.sql
+++ api/src/main/resources/db/migration/V5__add-items-cursos.sql
@@ -1,4 +1,4 @@
-INSERT INTO cursos (nombre, categoria) VALUES ('Primeros Pasos', 'Lógica de Programación');
-INSERT INTO cursos (nombre, categoria) VALUES ('HTML5 y CSS3 - parte 1', 'Primeras Páginas Web');
-INSERT INTO cursos (nombre, categoria) VALUES ('Git y Github', 'Git');
-INSERT INTO cursos (nombre, categoria) VALUES ('Java JRE y JDK', 'Java y Orientación a Objetos');
+INSERT INTO cursos (nombre, categoria) VALUES ('Logica de Programacion', 'Desarrollo');
+INSERT INTO cursos (nombre, categoria) VALUES ('Desarrollo Web', 'Java SDK');
+INSERT INTO cursos (nombre, categoria) VALUES ('Java EE', 'Rest API');
+INSERT INTO cursos (nombre, categoria) VALUES ('Api Rest Boot Spring', 'Rest API');
